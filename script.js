da8b04d50c10d07f6c14140c164ddf43b5b9e3b1
patorras <triotank@gmail.com>
2021-04-15 17:35:43 +0100

    made css kind of decent, and started some functions on JS

---
+++ script.js
@@ -0,0 +1,34 @@
+;
+
+
+function add (value1, value2) {
+    return value1 + value2;
+}
+
+function subtract (value1, value2) {
+    return value1 - value2;
+}
+
+function multiply (value1, value2) {
+    return value1 * value2;
+}
+
+function divide (value1, value2) {
+    return value1 / value2;
+}
+
+
+// press a button, stores that number 
+
+function buttonPress (number) {
+    
+    console.log(number);
+}
+
+// lets try, do a while loop, that while +-*/.= are not pressed just contenate the number string to the variable1
+
+// when +-*/ is pressed do a while loop, that while +-*/.= are not pressed just contenate the number string to the variable2,
+
+// press another button and stores the second number
+
+// press one of the symbols to calculate the two numbers and stores it
