6be456f103ca7847cc5f723bcc75aec2254223b5
patorras <triotank@gmail.com>
2021-05-10 20:31:19 +0100

    buttons presses seems to be working, now add '.'

--- test.js
+++ test.js
@@ -4,38 +4,98 @@ let value1 = "";
 let value2 ="";
 let operator = "";
 
+// if number key is pressed and operator === ""
+    // add to what is stored in the first value
 let keyPress = window.addEventListener('keypress', (e) => {
     let jsdf = document.getElementById("screen").textContent;
     
     if (/[0-9]/.test(Number(e.key))) {
-        return value1 = document.getElementById("screen").innerHTML = jsdf + e.key;
+
+        if(operator === "") {
+            return value1 = document.getElementById("screen").innerHTML = jsdf + e.key;
+        }
+        else if (operator === "+" ||operator === "-" || operator === "*" || operator === "/") {
+            return value2 = document.getElementById("screen").innerHTML = jsdf + e.key;
+        }
+        
     }  
 }) 
 
+// function () change operator
 let operation = window.addEventListener("keypress", (e) => {
-    if(e.key === "+") {
+    if(e.key === "+" ||e.key === "-" || e.key === "*" || e.key === "/") {
         console.log(e.key)
         document.getElementById("screen").innerText = "";
         return operator = e.key;
+    }
+    
+})
+
+// clear and backspace funtions
+let clear = window.addEventListener("keydown", (e) => {
+    if(e.key === "Delete") {
+        document.getElementById("screen").innerText = "";
+        value1 = "";
+        value2 = "";
+    }
+    else if (e.key === "Backspace") {
+        console.log(e);
+        
+        let val = document.getElementById("screen").textContent.slice(0, -1);
+        document.getElementById("screen").innerHTML = val;
+        if(operator === "") {
+            return value1 = val;
+        } 
+        else if (operator === "+" ||operator === "-" || operator === "*" || operator === "/") {
+            return value2 = val;
+        }
+    }
+})
+
+function sum (value1, value2) {
+    return Number(value1) + Number(value2);
+}
+
+function sub (value1, value2) {
+    return Number(value1) - Number(value2);
+}
+
+function mult (value1, value2) {
+    return Number(value1) * Number(value2);
+}
+
+function division (value1, value2) {
+    return Number(value1) / Number(value2);
+}
+
+//else if operator === "="
+// call function based in the operator variable
+let equal = window.addEventListener("keypress", (e) => {
+    if (e.key === "=" || e.key === "Enter") {
+        //if operator === "+"
+        // call sum function
+        if (operator === "+") {
+            document.getElementById("screen").innerHTML = sum(value1, value2)
+        }
+        else if (operator === "-") {
+            document.getElementById("screen").innerHTML = sub(value1, value2)
+        }
+        else if (operator  === "*") {
+            document.getElementById("screen").innerHTML = mult(value1, value2)
+        }
+        else if (operator === "/") {
+            document.getElementById("screen").innerHTML = division(value1, value2)
+        }
     }
 })
 
 
 
-function sum (value1, value2) {
-    return value1 + value2;
-}
-// if number key is pressed and operator === ""
-    // add to what is stored in the first value
-
-// else if operator === "+"
-    // call sum function
 
 // else if operator is pressed
     // change the operator variable
     // prepare for second value to be insrted
     
-// else if operator === "="
-    // call function based in the operator variable
+// 
 
-// function () change operator
+
